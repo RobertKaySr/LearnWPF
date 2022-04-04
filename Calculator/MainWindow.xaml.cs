0cbfb42ccb1b3c7137cfb608c8f1fc3210553b11
RobertKaySr <kayrobertchannel@gmail.com>
2022-04-04 04:01:15 +0200

    Initial Commit

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,62 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
         public MainWindow()
         {
             InitializeComponent();
+            acButton.Click += AcButton_Click;
+            twoButton.Click += TwoButton_Click;
+            minusButton.Click += MinusButton_Click;
+            zeroButton.Click += ZeroButton_Click;
+            plusNegativeButton.Click += PlusNegativeButton_Click;
+        }
+
+        private void PlusNegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+            else
+            {
+                resultLabel.Content = 15;
+            }
+        }
+
+        private void ZeroButton_Click(object sender, RoutedEventArgs e)
+        {
+            result = result * -2;
+        }
+
+        private void MinusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void TwoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void SevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "7";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}7";
+
+            }
         }
     }
 }
